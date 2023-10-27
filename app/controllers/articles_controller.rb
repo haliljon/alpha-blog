08b31f294f17e8e382c27e19ace2325ac85a486e
haliljon <juraboevhaliljon@gmail.com>
2023-10-27 17:23:20 +0500

    Add create, edit actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,28 @@ class ArticlesController<ApplicationController
     def new
         @article=Article.new
     end
+    
+    def edit
+        @article=Article.find(params[:id])
+    end
+
     def create 
         @article=Article.new(params.require(:article).permit(:title, :description))
         if @article.save
-        redirect_to article_path(@article)
+            flash[:notice] = "Article was created successfully."
+        redirect_to @article
         else
-            render 'new'
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @article=Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            render 'edit', status: :unprocessable_entity
         end
     end
 end
