38c3adbb03a54fb135b8c222a606ee909abe7605
haliljon <juraboevhaliljon@gmail.com>
2023-10-29 13:55:47 +0500

    Add one-to-many assossiations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,8 @@ class ArticlesController<ApplicationController
     end
 
     def create 
-        @article=Article.new(params.require(:article).permit(:title, :description))
+        @article=Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:notice] = "Article was created successfully."
         redirect_to @article
